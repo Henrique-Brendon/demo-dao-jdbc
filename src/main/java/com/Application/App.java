5d37f71cac21d08e754e737ed10be9521de320b0
henrique brendon <henriquebrendonti26@gmail.com>
2023-11-22 19:30:39 -0300

    Seller and Department entities classes

--- src/main/java/com/Application/App.java
+++ src/main/java/com/Application/App.java
@@ -1,8 +1,11 @@
 package com.Application;
 
 import java.sql.Connection;
+import java.util.Date;
 
 import com.jdbc.db.DB;
+import com.model.entities.Department;
+import com.model.entities.Seller;
 
 /**
  * Hello world!
@@ -10,9 +13,13 @@ import com.jdbc.db.DB;
  */
 public class App 
 {
-    
+
     public static void main( String[] args )
     {
-        Connection conn = DB.getConnection();
+        Department obj = new Department(1, "books");
+
+        Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        System.out.println(seller);
     }
 }
